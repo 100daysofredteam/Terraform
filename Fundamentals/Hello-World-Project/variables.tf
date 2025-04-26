d60a6011a2cc39baec538c7ad499e2ebbf780379
100daysofredteam <100daysofredteam@gmail.com>
2025-04-26 14:00:15 +0530

    Create variables.tf

---
+++ Fundamentals/Hello-World-Project/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+}
+
+variable "ami_id" {
+  description = "The AMI to use for EC2"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+}
