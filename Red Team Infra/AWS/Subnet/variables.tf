0de5bd4298ae4437cceacaf3cf7757fca000f3a8
100daysofredteam <100daysofredteam@gmail.com>
2025-05-10 19:50:50 +0530

    Create variables.tf

---
+++ Red Team Infra/AWS/Subnet/variables.tf
@@ -0,0 +1,73 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region to deploy infrastructure in"
+
+  validation {
+    condition     = length(trim(var.aws_region, " \t\n\r")) > 0
+    error_message = "The aws_region variable must not be empty."
+  }
+}
+
+variable "availability_zone" {
+  type        = string
+  description = "AZ to deploy the subnet in"
+
+  validation {
+    condition     = length(trim(var.availability_zone, " \t\n\r")) > 0
+    error_message = "The aavailability_zone variable must not be empty."
+  }
+}
+
+variable "aws_access_key" {
+  type        = string
+  description = "AWS Access Key"
+
+  validation {
+    condition     = length(trim(var.aws_access_key, " \t\n\r")) > 0
+    error_message = "The aws_access_key variable must not be empty."
+  }
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS Secret Key"
+  sensitive   = true
+
+  validation {
+    condition     = length(trim(var.aws_secret_key, " \t\n\r")) > 0
+    error_message = "The aws_secret_key variable must not be empty."
+  }
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block for the VPC"
+  default     = "10.10.0.0/16"
+
+  validation {
+    condition     = can(regex("^\\d+\\.\\d+\\.\\d+\\.\\d+/\\d+$", var.vpc_cidr))
+    error_message = "The VPC CIDR must be a valid CIDR range, e.g., 10.10.0.0/16"
+  }
+}
+
+variable "public_subnet_cidr" {
+  type        = string
+  description = "CIDR block for the Public Subnet"
+  default     = "10.10.1.0/24"
+
+  validation {
+    condition     = can(regex("^\\d+\\.\\d+\\.\\d+\\.\\d+/\\d+$", var.public_subnet_cidr))
+    error_message = "The CIDR must be a valid CIDR range, e.g., 10.10.0.0/16"
+  }
+}
+
+variable "private_subnet_cidr" {
+  type        = string
+  description = "CIDR block for the Private Subnet"
+  default     = "10.10.1.0/24"
+
+  validation {
+    condition     = can(regex("^\\d+\\.\\d+\\.\\d+\\.\\d+/\\d+$", var.private_subnet_cidr))
+    error_message = "The CIDR must be a valid CIDR range, e.g., 10.10.0.0/16"
+  }
+}
